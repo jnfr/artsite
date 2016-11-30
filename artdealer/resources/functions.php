0a4d450f7af3caae6b96c4e34294292329424680
Jnfr <jnfr.sd@gmail.com>
2016-11-30 13:36:00 -0500

    added admin area for art dealer users
    that grabs from database which painting that user is choosing to promote.  still need to add functionality to allow them to add and edit paintings they want to promote.  also worked on the display user page for art dealers that displays the paintings that they have chosen.

--- artdealer/resources/functions.php
+++ artdealer/resources/functions.php
@@ -250,6 +250,69 @@ DELIMETER;
 echo $painting;
     
 }}
+
+function get_promoted_paintings_for_admin(){
+
+$username = $_SESSION['username'];
+                       
+$query = query("SELECT * FROM promotions WHERE promoter_username = '{$username}' ");
+
+while($row = mysqli_fetch_array($query)){
+
+$promotions = <<<DELIMETER
+
+<a href="http://localhost/artdealer/public/painting.php?id={$row['art_number']}"><li>{$row['art_number']}</li></a>
+
+DELIMETER;
+
+echo $promotions;  
+}   
+}
+
+
+
+function get_promoted_paintings_for_display(){
+    
+$query = query("SELECT * FROM users WHERE user_number = " . escape_string($_GET['id']) . " "); 
+    
+while($row = mysqli_fetch_array($query)) {
+
+$username = $row['username'];
+    
+$b_query = query("SELECT * FROM promotions WHERE promoter_username = '{$username}' ");
+    
+ while($row = mysqli_fetch_array($b_query)){
+    
+    $art_number = $row['art_number'];
+    
+    $c_query = query("SELECT * FROM paintings WHERE painting_number = '{$art_number}' ");
+    
+    while($row = mysqli_fetch_array($c_query)){
+        
+$show_choices = <<<DELIMETER
+
+<div class="row">
+<div class="col-md-7">
+<a href="#">
+<img class="img-responsive" src="{$row['painting_image']}" alt="image of {$row['painting_title']}" height="200px">
+</a>
+</div>
+<div class="col-md-5">
+<h3>{$row['painting_title']}</h3>
+<h4>{$row['category']}, {$row['medium']}, {$row['size']}</h4>
+<p>{$row['description']}</p>
+<a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+</div>
+
+
+DELIMETER;
+echo $show_choices;
+    }
+   
+}
+   
+}}
                 
 
 ?>
